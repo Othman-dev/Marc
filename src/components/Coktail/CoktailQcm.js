0f9ec9c9e8714dd2b4db4e38167de696bf2a8762
naptracks <cesar.martel9@gmail.com>
2020-09-05 12:01:46 +0200

    not good but not bad

--- src/components/Coktail/CoktailQcm.js
+++ src/components/Coktail/CoktailQcm.js
@@ -17,20 +17,70 @@ const Coktail = props => {
         'display-flex': act === false
     })
 
+    const plus = classNames({
+        'dark': act === false,
+        'light': act
+    })
+
+    const onClick = type => {
+
+
+
+        if (type === 't') {
+            props.addTitle();
+        }
+        else if (type === 'c') {
+            props.addChapter();
+        }
+        else if (type === 'sc') {
+            props.addSubchapter();
+        }
+        else if (type === 'txt') {
+            props.addText();
+        }
+        else if (type === 'q') {
+            props.addQuote();
+        }
+        else if (type === 'img') {
+            props.addImage();
+        }
+        else if (type === 'ppp') {
+            props.addPpp();
+        }
+        else if (type === 'sep') {
+            props.addSeparation();
+        }
+        else if (type === 'que') {
+            props.addQuestion();
+        }
+        else if (type === 'del') {
+            props.delete();
+        }
+
+
+
+        setAct(true)
+    }
+
     return (
         <div className='coktail'>
             {
                 act ?
-                    <button className={props.className} onClick={handlePlus} ><i className="fas fa-plus"/></button> :
+                    <button className={plus} onClick={handlePlus} ><i className="fas fa-plus" /></button> :
 
                     <div className={classes}>
-                        <button onClick={handlePlus} ><i className="fas fa-times"/></button>
-                        <button onClick={props.addChapter}>C</button>
-                        <button onClick={props.addSubchapter}>sC</button>
-                        <button onClick={props.addText}><i className="fas fa-bars"/></button>
-                        <button onClick={props.addImage} ><i className="fas fa-camera"></i></button>
-                        <button onClick={props.addSeparation} >Séparation</button>
-                        <button><i className="fas fa-level-down-alt fa-rotate-90"/></button>
+                        <button onClick={handlePlus} ><i className="fas fa-times" /></button>
+                        <button onClick={() => onClick('t')}>T</button>
+                        <button onClick={() => onClick('c')}>C</button>
+                        <button onClick={() => onClick('sc')}>sC</button>
+                        <button onClick={() => onClick('txt')}><i className="fas fa-bars" /></button>
+                        <button onClick={() => onClick('q')}>Citation</button>
+                        <button onClick={() => onClick('img')} ><i className="fas fa-camera"></i></button>
+                        <button onClick={() => onClick('ppp')} ><i className="fas fa-file"></i></button>
+                        <button onClick={() => onClick('sep')} >Séparation</button>
+                        <button onClick={() => onClick('que')} >Question</button>
+                        <button onClick={() => onClick('del')} >Supprimer</button>
+                       
                     </div>
             }
 
