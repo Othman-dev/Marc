771bc2878ecff29e15f44e862ccd81b869dbffd4
naptracks <cesar.martel9@gmail.com>
2020-09-24 16:40:58 +0200

    hard code

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
 module.exports ={
-mongoURI: process.env.mongoURI
+mongoURI: 'mongodb+srv://cesar:%2FsWHyieZE%40bLNX%5E7X9E@marccluster.qznoh.gcp.mongodb.net/marcDB?retryWrites=true&w=majority'
+
 }
