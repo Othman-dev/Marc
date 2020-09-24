1d6d6fc2980a1778e739a63212ec47ee6955d237
naptracks <cesar.martel9@gmail.com>
2020-09-24 14:39:17 +0200

    heroku setup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const db = require('./config/keys').mongoURI;
-
+const path = require('path')
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,6 +19,17 @@ mongoose.Promise = global.Promise;
 
 app.use(express.json())
 app.use('/api', require('./routes/api'));
+
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 //error handling
 app.use(function(err, req, res, next){
